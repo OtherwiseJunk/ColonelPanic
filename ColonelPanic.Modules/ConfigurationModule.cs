f57574772236d0c095e5e6d1a3f4aa7edfaf7812
OtherwiseJunk <RobertWarrenDavid@gmail.com>
2017-09-17 00:42:40 -0400

    Add the Configuration module with the Change command.

---
+++ ColonelPanic.Modules/ConfigurationModule.cs
@@ -0,0 +1,32 @@
+﻿using ColonelPanic.Database.Contexts;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColonelPanic.Modules
+{
+    [Group("config"), RequireOwner]
+    class ConfigurationModule : ModuleBase
+    {
+        [Command("change"), Summary("Changes the specified configuration field to the specified value. Can currently update:\ngittoken\nbottoken\ngitcommit\n")]
+        public async Task Change([Summary("The field to update")] string field, [Summary("The new value")] string newValue)
+        {
+            switch (field)
+            {
+                case "gittoken":
+                    ConfigurationHandler.ChangeConfiguration(field, newValue);
+                    break;
+                case "bottoken":
+                    ConfigurationHandler.ChangeConfiguration(field, newValue);
+                    break;
+                case "gitcommit":
+                    ConfigurationHandler.ChangeConfiguration(field, newValue);
+                    break;                
+            }
+        }
+
+    }
+}
