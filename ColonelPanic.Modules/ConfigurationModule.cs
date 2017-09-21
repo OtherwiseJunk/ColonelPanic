90ea07e2b65fea5998ad454d34d8d617473dccd6
OtherwiseJunk <RobertWarrenDavid@gmail.com>
2017-09-21 18:30:30 -0400

    Added Requests class.

--- ColonelPanic.Modules/ConfigurationModule.cs
+++ ColonelPanic.Modules/ConfigurationModule.cs
@@ -12,7 +12,7 @@ namespace ColonelPanic.Modules
     public class ConfigurationModule : ModuleBase
     {
         [Command("change"), Summary("Changes the specified configuration field to the specified value. Can currently update:\ngittoken\nbottoken\ngitcommit\n")]
-        public async Task Change([Summary("The field to update")] string field, [Summary("The new value")] string newValue)
+        public static async Task Change([Summary("The field to update")] string field, [Summary("The new value")] string newValue)
         {
             switch (field)
             {
---
+++ WalkieTalkie/Requests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalkieTalkie
+{
+    public class Request
+    {
+        public string Source;
+        public string Timestamp;
+
+        public Request(string source)
+        {
+            Source = source;
+            Timestamp = DateTime.Now.ToString();
+        }
+
+    }
+
+    public class CommandRequest : Request
+    {
+        public string Command;
+        public string Arguments;
+
+        public CommandRequest(string command, string arguments, string source):base(source)
+        {           
+            Command = command;
+            Arguments = arguments;
+        }
+    }
+}
