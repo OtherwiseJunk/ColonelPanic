214ffc2aabbf64b7bf9d5700471f9f88502ca660
Bobby David <dartkietanmartaru@gmail.com>
2020-03-09 23:02:04 -0400

    Verify we have configuration before attempting to respond. Removed old file.

--- DartsDiscordBots/Modules/Pokemon/PokemonModule.cs
+++ DartsDiscordBots/Modules/Pokemon/PokemonModule.cs
@@ -16,69 +16,81 @@ namespace DartsDiscordBots.Modules
 
 		[Command("sword"), Summary("Adds you to the Pokemon Sword role.")]
 		public async Task AddSwordRole()
-		{
-			PokemonGuildConfiguration pokemonConfig = _pokemon.ConfiguredGuilds[Context.Guild.Id];
-			IRole SwordRole = Context.Guild.GetRole(pokemonConfig.SwordRoleId);
-			if (!(Context.User as IGuildUser).RoleIds.Contains(pokemonConfig.SwordRoleId))
+		{			
+			if (_pokemon.ConfiguredGuilds.ContainsKey(Context.Guild.Id))
 			{
-				await (Context.User as IGuildUser).AddRoleAsync(SwordRole);
-				await Context.Message.AddReactionAsync(Context.Guild.Emotes.First(emote => emote.Id == pokemonConfig.SwordEmoteId));
-				if ((Context.User as IGuildUser).RoleIds.Contains(pokemonConfig.SwordRoleId))
+				PokemonGuildConfiguration pokemonConfig = _pokemon.ConfiguredGuilds[Context.Guild.Id];
+				IRole SwordRole = Context.Guild.GetRole(pokemonConfig.SwordRoleId);
+				if (!(Context.User as IGuildUser).RoleIds.Contains(pokemonConfig.SwordRoleId))
 				{
-					await Context.Channel.SendFileAsync(GetImageStreamFromBase64(_pokemon.TwoPokemonSwordImage), "TWOPokemonz! - Sword");
+					await (Context.User as IGuildUser).AddRoleAsync(SwordRole);
+					await Context.Message.AddReactionAsync(Context.Guild.Emotes.First(emote => emote.Id == pokemonConfig.SwordEmoteId));
+					if ((Context.User as IGuildUser).RoleIds.Contains(pokemonConfig.SwordRoleId))
+					{
+						await Context.Channel.SendFileAsync(GetImageStreamFromBase64(_pokemon.TwoPokemonSwordImage), "TWOPokemonz! - Sword");
+					}
+				}
+				else
+				{
+					await Context.Channel.SendMessageAsync("You're already in the role!");
 				}
 			}
-			else
-			{
-				await Context.Channel.SendMessageAsync("You're already in the role!");
-			}
+			else await Context.Channel.SendMessageAsync("Sorry, this guild is not configured for the Pokemon module.");
 		}
 
 		[Command("shield"), Summary("Adds you to the Pokemon Shield role.")]
 		public async Task AddShieldRole()
 		{
-			PokemonGuildConfiguration pokemonConfig = _pokemon.ConfiguredGuilds[Context.Guild.Id];
-			IRole ShieldRole = Context.Guild.GetRole(pokemonConfig.ShieldRoleId);
-			if (!(Context.User as IGuildUser).RoleIds.Contains(pokemonConfig.ShieldRoleId))
+			if (_pokemon.ConfiguredGuilds.ContainsKey(Context.Guild.Id))
 			{
-				await (Context.User as IGuildUser).AddRoleAsync(ShieldRole);
-				await Context.Message.AddReactionAsync(Context.Guild.Emotes.First(emote => emote.Id == pokemonConfig.ShieldEmoteId));
-				if ((Context.User as IGuildUser).RoleIds.Contains(pokemonConfig.ShieldRoleId))
+				PokemonGuildConfiguration pokemonConfig = _pokemon.ConfiguredGuilds[Context.Guild.Id];
+				IRole ShieldRole = Context.Guild.GetRole(pokemonConfig.ShieldRoleId);
+				if (!(Context.User as IGuildUser).RoleIds.Contains(pokemonConfig.ShieldRoleId))
 				{
-					await Context.Channel.SendFileAsync(GetImageStreamFromBase64(_pokemon.TwoPokemonShieldImage), "TWOPokemonz! - Shield");
+					await (Context.User as IGuildUser).AddRoleAsync(ShieldRole);
+					await Context.Message.AddReactionAsync(Context.Guild.Emotes.First(emote => emote.Id == pokemonConfig.ShieldEmoteId));
+					if ((Context.User as IGuildUser).RoleIds.Contains(pokemonConfig.ShieldRoleId))
+					{
+						await Context.Channel.SendFileAsync(GetImageStreamFromBase64(_pokemon.TwoPokemonShieldImage), "TWOPokemonz! - Shield");
+					}
+				}
+				else
+				{
+					await Context.Channel.SendMessageAsync("You're already in the role!");
 				}
 			}
-			else
-			{
-				await Context.Channel.SendMessageAsync("You're already in the role!");
-			}
+			else await Context.Channel.SendMessageAsync("Sorry, this guild is not configured for the Pokemon module.");
 		}
 
 		[Command("ididit"), Summary("Marks you as a LEAGUE CHAMPION.")]
 		public async Task DesignateAsChampion()
 		{
-			PokemonGuildConfiguration pokemonConfig = _pokemon.ConfiguredGuilds[Context.Guild.Id];
-			IGuildUser user = Context.User as IGuildUser;
-			if (!user.RoleIds.Contains(pokemonConfig.ShieldChampionRoleId) && !user.RoleIds.Contains(pokemonConfig.SwordChampionRoleId))
+			if (_pokemon.ConfiguredGuilds.ContainsKey(Context.Guild.Id))
 			{
-				if (user.RoleIds.Contains(pokemonConfig.ShieldRoleId) || user.RoleIds.Contains(pokemonConfig.SwordRoleId))
+				PokemonGuildConfiguration pokemonConfig = _pokemon.ConfiguredGuilds[Context.Guild.Id];
+				IGuildUser user = Context.User as IGuildUser;
+				if (!user.RoleIds.Contains(pokemonConfig.ShieldChampionRoleId) && !user.RoleIds.Contains(pokemonConfig.SwordChampionRoleId))
 				{
-					AddVersionChampionship(user, Context, pokemonConfig);
-					if (user.RoleIds.Contains(pokemonConfig.GenericChampionRoleId))
+					if (user.RoleIds.Contains(pokemonConfig.ShieldRoleId) || user.RoleIds.Contains(pokemonConfig.SwordRoleId))
 					{
-						await user.RemoveRoleAsync(Context.Guild.GetRole(pokemonConfig.GenericChampionRoleId));
+						AddVersionChampionship(user, Context, pokemonConfig);
+						if (user.RoleIds.Contains(pokemonConfig.GenericChampionRoleId))
+						{
+							await user.RemoveRoleAsync(Context.Guild.GetRole(pokemonConfig.GenericChampionRoleId));
+						}
 					}
+					else
+					{
+						await user.AddRoleAsync(Context.Guild.GetRole(pokemonConfig.GenericChampionRoleId));
+					}
+					await Context.Message.AddReactionAsync(Context.Guild.Emotes.First(e => e.Id == pokemonConfig.LeagueChampionEmoteId));
 				}
 				else
 				{
-					await user.AddRoleAsync(Context.Guild.GetRole(pokemonConfig.GenericChampionRoleId));
+					await Context.Channel.SendMessageAsync("We already know you're a champ. Showoff.");
 				}
-				await Context.Message.AddReactionAsync(Context.Guild.Emotes.First(e => e.Id == pokemonConfig.LeagueChampionEmoteId));
 			}
-			else
-			{
-				await Context.Channel.SendMessageAsync("We already know you're a champ. Showoff.");
-			}
+			else await Context.Channel.SendMessageAsync("Sorry, this guild is not configured for the Pokemon module.");
 		}
 
 		private void AddVersionChampionship(IGuildUser user, ICommandContext context, PokemonGuildConfiguration pokemonConfig)
