2eac9cb7d5c3779a9022e3a6e53b1ddc6458e30b
Bobby David <dartkietanmartaru@gmail.com>
2022-05-17 20:14:16 -0400

    Revert environmental variable name change

--- ColonelPanic/Constants/ConnectionStrings.cs
+++ ColonelPanic/Constants/ConnectionStrings.cs
@@ -6,6 +6,6 @@ namespace ColonelPanic.Constants
 {
 	public class ConnectionStrings
 	{
-		public static string ConnectionString = Environment.GetEnvironmentVariable("COLONELDB") == null ? "data source=EPSILON;initial catalog=MotherBrain;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework" : Environment.GetEnvironmentVariable("COLONELDB");
+		public static string ConnectionString = Environment.GetEnvironmentVariable("DATABASE") == null ? "data source=EPSILON;initial catalog=MotherBrain;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework" : Environment.GetEnvironmentVariable("DATABASE");
 	}
 }
